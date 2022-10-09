bcdd6753ccacfaa134516d09bc91ba1f45a573c9
Zack Kollar <rallokkcaz@gmail.com>
2022-10-08 19:09:06 -0700

    :hammer: Enabled getting the state of the providers

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,15 @@ use tokio::sync::{broadcast, mpsc, Mutex};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ProviderState<T> {
+    pub name: String,
     pub status: bool,
     pub last_seen: Option<T>,
 }
 
-impl<T> Default for ProviderState<T> {
-    fn default() -> Self {
+impl<T> ProviderState<T> {
+    fn new(name: impl Into<String>) -> Self {
         Self {
+            name: name.into(),
             status: false,
             last_seen: None,
         }
@@ -32,9 +34,9 @@ where
     T: Clone + Debug + Send + Sync + 'static,
     S: Stream<Item = Result<T>> + Send + Sync,
 {
-    pub fn new(outbound: mpsc::UnboundedSender<T>, stream: S) -> Self {
+    pub fn new(name: String, outbound: mpsc::UnboundedSender<T>, stream: S) -> Self {
         Self {
-            state: Arc::new(Mutex::new(ProviderState::default())),
+            state: Arc::new(Mutex::new(ProviderState::new(name))),
             stream: Box::pin(stream),
             outbound,
         }
@@ -83,9 +85,16 @@ where
         }
     }
 
-    pub fn add_provider_stream(&mut self, stream: S) {
+    pub fn add_provider_stream(&mut self, name: impl Into<String>, stream: S) {
         self.providers
-            .push(Provider::new(self.outbound.clone(), stream));
+            .push(Provider::new(name.into(), self.outbound.clone(), stream));
+    }
+
+    pub fn get_provider_states(&self) -> Vec<Arc<Mutex<ProviderState<T>>>> {
+        self.providers
+            .iter()
+            .map(|provider| provider.state.clone())
+            .collect::<Vec<_>>()
     }
 
     pub async fn produce(&mut self) -> Result<()> {
@@ -211,6 +220,47 @@ mod tests {
 
     use super::*;
 
+    // Helpers
+    pub fn is_sorted<T: IntoIterator>(t: T) -> bool
+    where
+        <T as IntoIterator>::Item: std::cmp::PartialOrd,
+    {
+        let mut iter = t.into_iter();
+
+        if let Some(first) = iter.next() {
+            iter.try_fold(first, |previous, current| {
+                if previous > current {
+                    Err(())
+                } else {
+                    Ok(current)
+                }
+            })
+            .is_ok()
+        } else {
+            true
+        }
+    }
+
+    pub fn no_sequential_duplicates<T: IntoIterator>(t: T) -> bool
+    where
+        <T as IntoIterator>::Item: std::cmp::PartialEq,
+    {
+        let mut iter = t.into_iter();
+
+        if let Some(first) = iter.next() {
+            iter.try_fold(first, |previous, current| {
+                if previous == current {
+                    Err(())
+                } else {
+                    Ok(current)
+                }
+            })
+            .is_ok()
+        } else {
+            true
+        }
+    }
+
     #[tokio::test]
     async fn system_architecture() {
         #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
@@ -235,14 +285,19 @@ mod tests {
         let (provider_tx, provider_rx) = mpsc::unbounded_channel::<Block>();
         let mut provider_system = ProviderSystem::new(provider_tx);
 
-        provider_system.add_provider_stream(s0);
-        provider_system.add_provider_stream(s1);
-        provider_system.add_provider_stream(s2);
+        provider_system.add_provider_stream("stream_1", s0);
+        provider_system.add_provider_stream("stream_2", s1);
+        provider_system.add_provider_stream("stream_3", s2);
 
+        // Get information about each provider
+        let provider_states = provider_system.get_provider_states();
+
+        // Produce data from each provider into the sequencer
         let provider_system_handle = tokio::spawn(async move {
             provider_system.produce().await.unwrap();
         });
 
+        // Take in a stream of data from the providers and produce a stream of dedup'd sorted data
         let (sequencer_tx, sequencer_rx) = mpsc::unbounded_channel();
         let mut sequencer = Sequencer::new(provider_rx, sequencer_tx, 1).unwrap();
 
@@ -250,6 +305,7 @@ mod tests {
             sequencer.consume().await.unwrap();
         });
 
+        // Take in a stream of data and fan it out to all subscribers
         let (dispatcher_tx, _dispatcher_rx) = broadcast::channel(10);
         let mut dispatcher = Dispatcher::new(sequencer_rx, dispatcher_tx.clone());
 
@@ -257,30 +313,44 @@ mod tests {
             dispatcher.fanout().await.unwrap();
         });
 
+        // Assert these two subscribers receive a dedup'd sequence of blocks.
         let mut assertion_handle_rx = dispatcher_tx.subscribe();
         let assertion_handle_0 = tokio::spawn(async move {
-            let expected = vec![Block(0), Block(1), Block(2), Block(3), Block(4), Block(5)];
+            let mut received = vec![];
             while let Ok(item) = assertion_handle_rx.try_recv() {
-                println!("Process #1 received item: {:?}", item);
-                assert!(item <= *expected.iter().last().unwrap());
+                // println!("Subscriber #1 received item: {:?}", item);
+                received.push(item);
             }
+            assert!(is_sorted(&received));
+            assert!(no_sequential_duplicates(&received));
         });
-
         let mut assertion_handle_rx = dispatcher_tx.subscribe();
         let assertion_handle_1 = tokio::spawn(async move {
-            let expected = vec![Block(0), Block(1), Block(2), Block(3), Block(4), Block(5)];
+            let mut received = vec![];
             while let Ok(item) = assertion_handle_rx.try_recv() {
-                println!("Process #2 received item: {:?}", item);
-                assert!(item <= *expected.iter().last().unwrap());
+                // println!("Subscriber #2 received item: {:?}", item);
+                received.push(item);
             }
+            assert!(is_sorted(&received));
+            assert!(no_sequential_duplicates(&received));
         });
 
+        // Join em up.
         let _ = tokio::try_join!(
             provider_system_handle,
             sequencer_handle,
             dispatcher_handle,
             assertion_handle_0,
             assertion_handle_1
-        );
+        )
+        .unwrap();
+
+        // Check that all providers are in the expected state
+        let expected_states = vec![(true, Block(1)), (false, Block(3)), (true, Block(5))];
+        for (i, provider_state) in provider_states.iter().enumerate() {
+            let provider_state = provider_state.lock().await;
+            assert_eq!(provider_state.status, expected_states[i].0);
+            assert_eq!(provider_state.last_seen, Some(expected_states[i].1));
+        }
     }
 }
